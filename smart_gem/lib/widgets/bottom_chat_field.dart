a4c8694f2e733566fa38bcf04292e4497f50f2fc
W. Shashindu Chamika <126416494+WShashinduChamika@users.noreply.github.com>
2024-07-12 21:11:27 +0530

    Developed preview images part for bottom chat field

--- smart_gem/lib/widgets/bottom_chat_field.dart
+++ smart_gem/lib/widgets/bottom_chat_field.dart
@@ -3,7 +3,9 @@ import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:smart_gem/providers/chat_provider.dart';
+import 'package:smart_gem/widgets/preview_images.dart';
 
 class BottomChatField extends StatefulWidget {
   const BottomChatField({super.key, required this.chatProvider});
@@ -17,6 +19,8 @@ class _BottomChatFieldState extends State<BottomChatField> {
 
   final FocusNode textFieldFocusNode = FocusNode();
 
+  final ImagePicker imagePicker = ImagePicker();
+
   @override
   void dispose() {
     // TODO: implement dispose
@@ -24,12 +28,11 @@ class _BottomChatFieldState extends State<BottomChatField> {
     textFieldFocusNode.dispose();
     super.dispose();
   }
-  
-  Future<void> sendMessage({
-    required String message,
-    required ChatProvider chatProvider,
-    required bool isTextOnly
-  }) async {
+
+  Future<void> sendMessage(
+      {required String message,
+      required ChatProvider chatProvider,
+      required bool isTextOnly}) async {
     //send message
     try {
       await chatProvider.sendMessage(
@@ -38,14 +41,31 @@ class _BottomChatFieldState extends State<BottomChatField> {
       );
     } catch (e) {
       print('eror : $e ');
-
     } finally {
       messageController.clear();
       textFieldFocusNode.unfocus();
     }
   }
+
+  //pick an image
+  void pickImage() async {
+    try {
+      final pickedImages = await imagePicker.pickMultiImage(
+        maxHeight: 800,
+        maxWidth: 800,
+        imageQuality: 95,
+      );
+      widget.chatProvider.setImageFilesList(filesList: pickedImages!);
+    } catch (e) {
+      print('error : $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    bool hasImages = widget.chatProvider.imageFilesList.isNotEmpty &&
+        widget.chatProvider.imageFilesList != null;
+
     return Container(
       decoration: BoxDecoration(
         color: Theme.of(context).cardColor,
@@ -55,63 +75,73 @@ class _BottomChatFieldState extends State<BottomChatField> {
           width: 1,
         ),
       ),
-      child: Row(
+      child: Column(
         children: [
-          IconButton(
-            onPressed: () {
-              //send image
-            },
-            icon: const Icon(Icons.image),
-          ),
-          Expanded(
-            child: TextField(
-              focusNode: textFieldFocusNode,
-              controller: messageController,
-              textInputAction: TextInputAction.send,
-              onSubmitted: (String value) {
-                //chatProvider.sendMessage();
-                if(value.isNotEmpty){
-                sendMessage(
-                  message: messageController.text,
-                  chatProvider: widget.chatProvider,
-                  isTextOnly: true,
-                );
-              }
-              },
-              decoration: InputDecoration.collapsed(
-                hintText: "Enter your prompt here...",
-                border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.circular(30),
+          if(hasImages) const PreviewImage(),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  //send image
+                  if(hasImages){
+                     widget.chatProvider.setImageFilesList(filesList: []);
+                  }else{
+                    pickImage();
+                  }
+                },
+                icon: Icon(hasImages? Icons.delete : Icons.image),
+              ),
+              Expanded(
+                child: TextField(
+                  focusNode: textFieldFocusNode,
+                  controller: messageController,
+                  textInputAction: TextInputAction.send,
+                  onSubmitted: (String value) {
+                    //chatProvider.sendMessage();
+                    if (value.isNotEmpty) {
+                      sendMessage(
+                        message: messageController.text,
+                        chatProvider: widget.chatProvider,
+                        isTextOnly: true,
+                      );
+                    }
+                  },
+                  decoration: InputDecoration.collapsed(
+                    hintText: "Enter your prompt here...",
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              //send message
-              if(messageController.text.isNotEmpty){
-                sendMessage(
-                  message: messageController.text,
-                  chatProvider: widget.chatProvider,
-                  isTextOnly: true,
-                );
-              }
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.deepPurple,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              margin: const EdgeInsets.all(8),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Icon(
-                  Icons.arrow_upward,
-                  color: Colors.white,
+              GestureDetector(
+                onTap: () {
+                  //send message
+                  if (messageController.text.isNotEmpty) {
+                    sendMessage(
+                      message: messageController.text,
+                      chatProvider: widget.chatProvider,
+                      isTextOnly: true,
+                    );
+                  }
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  margin: const EdgeInsets.all(8),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.arrow_upward,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
               ),
-            ),
+            ],
           ),
         ],
       ),
